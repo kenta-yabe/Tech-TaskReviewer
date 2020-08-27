87ad89984e03c4225660f3209fb61beab7c1e8e3
kenta.yabe <kenta.yabe.0127@gmail.com>
2020-08-27 03:44:24 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-   root to: 'toppages/index'
-  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+    root to: 'toppages#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:show, :new, :create]
 end
