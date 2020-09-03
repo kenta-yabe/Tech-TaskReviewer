597ffd67a11d66a6c3040c8ba2471cfcc509a0bc
kenta.yabe <kenta.yabe.0127@gmail.com>
2020-09-03 12:26:57 +0000

    fix design

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -3,9 +3,16 @@ class Purpose < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 30 }
   
+ # validate  :start_not_before_end
+  
   has_many :tasks, dependent: :destroy
   
   ####### reviews ###################
   has_many :reviews, dependent: :destroy
   has_many :review_users, through: :reviews, source: :user
+  
+  ####################################
+ # def start_not_before_end
+  #  errors.add(:end_day, "は開始日以降のものを選択してください")
+  #end
 end
